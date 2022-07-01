f39d018d8471ce45f842402eb2a14b512ac34e71
wonpanu <won.panumas@gmail.com>
2022-07-01 12:36:02 +0700

    refactor code

--- pkg/repo/blog.go
+++ pkg/repo/blog.go
@@ -6,18 +6,22 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type BlogRepo struct {
+type BlogRepo interface {
+	GetAll() ([]entity.Blog, error)
+}
+
+type MongoDB struct {
 	coll *mgm.Collection
 }
 
-func (b *BlogRepo) GetAll() ([]entity.Blog, error) {
+func (b MongoDB) GetAll() ([]entity.Blog, error) {
 	blogs := []entity.Blog{}
 	err := b.coll.SimpleFind(&blogs, bson.M{})
 	return blogs, err
 }
 
 func NewBlogRepo(coll *mgm.Collection) BlogRepo {
-	return BlogRepo{
+	return MongoDB{
 		coll: coll,
 	}
 }
